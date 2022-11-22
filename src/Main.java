2b9740b210bb2b673d31cd1825c4ab005b649330
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-11-22 20:05:04 +0500

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,29 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке ...");
         }
 
+        // Разделитель
+        System.out.println();
+
+        // Задание 2
+        int clientDeviceYear = 2013;
+
+        if (clientDeviceYear < 2015){
+
+            if (clientOS == 0){
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке ...");
+            } else if (clientOS == 1){
+                System.out.println("Установите облегченную версию приложения для Android по ссылке ...");
+            }
+
+        } else {
+
+            if (clientOS == 0){
+                System.out.println("Установите версию приложения для iOS по ссылке ...");
+            } else if (clientOS == 1){
+                System.out.println("Установите версию приложения для Android по ссылке ...");
+            }
+
+        }
+
     }
 }
